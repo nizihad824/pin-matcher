a403747362e8171775a22670833cf84318b667b7
nizihad824 <mdnurulislam97@gmail.com>
2022-02-17 19:12:03 +0100

    type-button

--- js/app.js
+++ js/app.js
@@ -1,13 +1,25 @@
 function getPin(){
     const pin = Math.round(Math.random()*10000);
     const pinString = pin +'';
-    if(pinString.length==4){
+    if(pinString.length == 4){
+        return pin;
+    }
+    else {
+         console.log('got 3 digit and calling again',pin);
         return pin;
     }
 }
 
 
 function generatePin(){
+    const pin =getPin();
+    document.getElementById('display-pin').value =pin;
+} 
 
-    console.log('generating')
-}
+
+document.getElementById('key-pad').addEventListener('click',function(event){
+    console.log("any key clicked")
+
+    
+
+})
