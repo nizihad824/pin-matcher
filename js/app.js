cbd0aa15f3c710aee0b65d8f3e890ed216edf1ff
nizihad824 <mdnurulislam97@gmail.com>
2022-02-18 11:32:31 +0100

    notify pin

--- js/app.js
+++ js/app.js
@@ -21,11 +21,37 @@ document.getElementById('key-pad').addEventListener('click',function(event){
 
     const number = event.target.innerText;
     // console.log("any key clicked")
-    const calc =document.getElementById('typed-numbers');
-    calc.value = number;
-    
+    const calcInput =document.getElementById('typed-numbers');
+    if(isNaN(number)){
+       
+        if(number == 'C'){
+            calcInput.value = '';
 
+        }
+    }
+    else{
+    const previousNumber = calcInput.value;
+    const newNumber = previousNumber + number;calcInput.value = newNumber;
 
-    
-
+    }
 })
+function verifyPin(){
+    // console.log('verifying pin')
+    const pin =document.getElementById('display-pin').value;
+    const typedNumbers = document.getElementById('typed-numbers').value;
+    const successMessage = document.getElementById('notify-success');
+    const failError = document.getElementById('notify-fail');
+
+    if(pin == typedNumbers){
+        
+        successMessage.style.display = 'block';
+        failError.style.display = 'none'
+    }
+    else {
+        successMessage.style.display = 'none';
+        failError.style.display = 'block'
+        
+        
+    }
+
+}
